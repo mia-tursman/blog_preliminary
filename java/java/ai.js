74592318345e4ffbbf365de94ae1c430fc08a4b3
mia-tursman <mtursman@umich.edu>
2026-05-24 09:40:49 -0400

    Add files via upload

---
+++ java/java/ai.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides(slideIndex);
+});
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (slides.length === 0) {
+    return;
+  }
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  if (dots.length > 0) {
+    dots[slideIndex - 1].classList.add("active");
+  }
+}
